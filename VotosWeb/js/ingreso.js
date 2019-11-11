ec0e81efa582bfcdd51e6ee66fbe072f7e8eb968
CamiloGomez <cgcamilogomez@gmail.com>
2019-11-11 15:23:33 -0500

    Se agrega funcion para consumir api

--- VotosWeb/js/ingreso.js
+++ VotosWeb/js/ingreso.js
@@ -4,19 +4,35 @@ var imput = document.getElementById('inputCedula');
 var valueInput;
 btn.addEventListener('click', function () {
     valueInput = imput.value
-    if (valueInput == "") {
-        alert("Por favor ingresar un valor");
-    }
-    else {
-        //Es esta parte se invoca al metodo que realiza la petición al servidor
-        // para validar que el usuairo que ingresa tiene número de cedula
-        alert(valueInput);
-        window.open("votos.html");
-        //var newUrl = "votos.html";
-        //document.location.href = newUrl;
-        //window.open (location.assign("file:///D:/Proyecto/VotosWeb/votos.html"));
-        //document.location.replace("file:///D:/Proyecto/VotosWeb/votos.html");    
-    }
+    obtenerDatos();
+    // if (valueInput == "") {
+    //     alert("Por favor ingresar un valor");
+    // }
+    // else {
+    //     //Es esta parte se invoca al metodo que realiza la petición al servidor
+    //     // para validar que el usuairo que ingresa tiene número de cedula
+    //     alert(valueInput);
+    //     window.open("votos.html");
+    //     //var newUrl = "votos.html";
+    //     //document.location.href = newUrl;
+    //     //window.open (location.assign("file:///D:/Proyecto/VotosWeb/votos.html"));
+    //     //document.location.replace("file:///D:/Proyecto/VotosWeb/votos.html");    
+    // }
+
 
     
 });
+
+function obtenerDatos(){
+    let url `http://192.168.1.4:52861/api/Electores/`;
+
+    const api = new XMLHttpRequest();
+    api.open('GET', URL, true)
+    api.send();
+    
+    api.onreadystatechange = function(){
+        if (this.status == 200 && this.readyState == 4){
+            let datos = JSON.parse(this.responseText);
+        }
+    }
+}
